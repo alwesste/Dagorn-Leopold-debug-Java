2733ae7478e8b2729888fd91e19003e7059dc38e
Leopold <leopolddag@gmail.com>
2024-09-28 13:37:29 +0200

    Removing the filter and few comment

--- Project02Eclipse/src/com/hemebiotech/analytics/treeMap.java
+++
@@ -1,9 +0,0 @@
-package com.hemebiotech.analytics;
-
-import java.util.List;
-import java.util.Map;
-
-public class treeMap implements Map<String, Integer> {
-    public treeMap(List<String> symptoms) {
-    }
-}
